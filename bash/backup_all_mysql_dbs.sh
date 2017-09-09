d359a270bda0532c8d3ff5389ccc86819e3b2430
Victor Miti <victormiti@umusebo.com>
2017-09-09 16:38:41 +0200

    refactored the mysqldump script

--- bash/backup_all_mysql_dbs.sh
+++ bash/backup_all_mysql_dbs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 TIMESTAMP=$(date +"%F")
-BACKUP_DIR="~/_BACKUP/mysql/$TIMESTAMP"
+BACKUP_DIR="$HOME/_BACKUP/mysql/$TIMESTAMP"
  
 mkdir -p "$BACKUP_DIR/mysql"
  
-databases=`sudo mysql --defaults-extra-file=/root/.my.cnf --batch --skip-column-names -e "SHOW DATABASES;" | grep -E -v "(information|performance)_schema"`
+databases=`mysql --batch --skip-column-names -e "SHOW DATABASES;" | grep -E -v "(information|performance)_schema"`
  
 for db in $databases; do
-  mysqldump --force --opt --defaults-extra-file=/root/.my.cnf --databases $db | gzip > "$BACKUP_DIR/$db.gz"
+  mysqldump --force --opt --databases $db | gzip > "$BACKUP_DIR/$db`date +%Y%m%d`.gz"
 done
