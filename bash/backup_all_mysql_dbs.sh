fffa38c48460adec24c63cced0c0428b298a9cc0
Victor Miti <victormiti@umusebo.com>
2017-09-09 17:48:44 +0200

    fixed several bugs in my MySQL backup script

--- bash/backup_all_mysql_dbs.sh
+++ bash/backup_all_mysql_dbs.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
- 
-TIMESTAMP=$(date +"%F")
-BACKUP_DIR="$HOME/_BACKUP/mysql/$TIMESTAMP"
- 
-mkdir -p "$BACKUP_DIR/mysql"
- 
+
+# Assumes that your setup is configured such that mysql and mysqldump don't
+# prompt you for password. This is very useful for running cron jobs.
+# See https://stackoverflow.com/a/19913912/ for how to achieve this.
+# Also see https://dev.mysql.com/doc/refman/5.7/en/option-files.html
+
+DIR_TIMESTAMP=$(date +"%Y-%h-%d")
+BACKUP_DIR="Your/Backup/Dir/DB/MySQL/$DIR_TIMESTAMP"
+
+mkdir -p "$BACKUP_DIR"
+
 databases=`mysql --batch --skip-column-names -e "SHOW DATABASES;" | grep -E -v "(information|performance)_schema"`
- 
+
 for db in $databases; do
-  mysqldump --force --opt --databases $db | gzip > "$BACKUP_DIR/$db`date +%Y%m%d`.gz"
+  FILE_TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+  mysqldump --force --opt --databases $db | gzip > "${BACKUP_DIR}/${db}_${FILE_TIMESTAMP}.gz"
 done
